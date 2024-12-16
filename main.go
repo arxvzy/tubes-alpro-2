870d8863df34f2a3924d98a07fc93c7ba0675628
Arxvzy <szaryabima@gmail.com>
2024-12-16 20:14:35 +0700

    refactor: add transaction struct

--- main.go
+++ main.go
@@ -9,16 +9,23 @@ const NMAX int = 10
 type Barang struct {
 	Nama     string `json:"nama"`
 	Harga    int    `json:"harga"`
-	Stok     int    `json:"stok"`
 	Terjual  int    `json:"terjual"`
 	Kategori string `json:"kategori"`
 }
 
+type Transaksi struct {
+	Harga int `json:"harga"`
+}
+
+type Data struct {
+	Count  int            `json:"count"`
+	Barang [NMAX]Barang   `json:"barang"`
+	Harga  [100]Transaksi `json:"harga"`
+}
+
 func main() {
-
-	var barang [NMAX]Barang
+	var data Data = getData()
 	var inputMenu string
-
 	for inputMenu != "0" {
 		clearScreen()
 		fmt.Println("Aplikasi Penjualan")
@@ -26,17 +33,19 @@ func main() {
 		fmt.Println("2. Tambah Barang")
 		fmt.Println("3. Edit Barang")
 		fmt.Println("4. Hapus Barang")
-		fmt.Println("5. Kelola Transaksi Penjualan") // tambah, edit, hapus transaksi, kembali ke menu utama
-		fmt.Println("6. Laporan Penjualan")          // top 5 penjualan, data modal, pendapatan, dll
+		fmt.Println("5. Transaksi")         // tambah, edit, hapus transaksi, kembali ke menu utama
+		fmt.Println("6. Laporan Penjualan") // top 5 penjualan, data modal, pendapatan, dll
 		fmt.Println("0. Keluar")
 		fmt.Print("Pilihan anda : ")
 		fmt.Scanln(&inputMenu)
 
 		switch inputMenu {
 		case "1":
-			lihatBarang()
+			lihatBarang(data)
 		case "2":
-			writeBarang(barang)
+			tambahBarang(&data)
 		}
 	}
 }
+
+// harga modal pada input barang,
