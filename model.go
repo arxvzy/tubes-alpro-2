ee4276be5ba9d6b11340f0d2db7d7a83b18bc756
Arxvzy <szaryabima@gmail.com>
2024-12-16 20:16:21 +0700

    refactor: remove useless line

--- model.go
+++ model.go
@@ -6,24 +6,24 @@ import (
 	"os"
 )
 
-func getBarang() [10]Barang {
+func getData() Data {
 	var raw, err = os.ReadFile("barang.bin")
 	if err != nil {
 		fmt.Println(err)
 	}
-	var barang [10]Barang
-	json.Unmarshal(raw, &barang)
-	return barang
+	var data Data
+	json.Unmarshal(raw, &data)
+	return data
 }
 
-func writeBarang(barang [10]Barang) {
-	var raw, err = json.Marshal(barang)
+func writeBarang(data Data) error {
+	var raw, err = json.Marshal(data)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(raw)
 	err = os.WriteFile("barang.bin", raw, 0644)
 	if err != nil {
 		fmt.Println(err)
 	}
+	return err
 }
